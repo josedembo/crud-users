9d04eee26eaf410b4db239cc42e3bd2e85047f67
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-04 06:43:58 -0300

    cria o servico de cadastro de usuario

---
+++ src/service/user/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { CreateUserDTOS } from "../../dtos/CreateUserDTOS";
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../../repositories/UserRepositories";
+import { User } from "../../entity/User";
+
+class CreateUserService {
+
+    async execute({ name, username, email, password, phone }: CreateUserDTOS): Promise<User> {
+
+        const userRepositories = getCustomRepository(UserRepositories);
+
+        const existUser = await userRepositories.find({ where: { username, email } });
+
+        if (existUser) {
+            throw new Error("user already exists");
+        }
+
+        const newUser = userRepositories.create({
+            name,
+            username,
+            email,
+            password,
+            phone
+        });
+
+        const user = await userRepositories.save(newUser);
+
+        return user;
+
+    }
+}
+
+export { CreateUserService }
