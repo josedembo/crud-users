8308764e58d0934213ff5e33106c57f3a9acd356
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-07 08:14:24 -0300

    actualiza a logica de verificacao de usuario existente

--- src/service/user/CreateUserService.ts
+++ src/service/user/CreateUserService.ts
@@ -1,4 +1,4 @@
-import { CreateUserDTO} from "../../dtos/CreateUserDTO";
+import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 import { getCustomRepository } from "typeorm";
 import { UserRepositories } from "../../repositories/UserRepositories";
 import { User } from "../../entity/User";
@@ -11,7 +11,7 @@ class CreateUserService {
 
         const existUser = await userRepositories.find({ where: { username, email } });
 
-        if (existUser) {
+        if (existUser.length != 0) {
             throw new Error("user already exists");
         }
 
