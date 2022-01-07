dd293df62d18f7c998970534e994cfeab3e481a1
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-07 08:20:25 -0300

    inicaliza o servidor

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import "./database";
+import express from "express";
+import { routes } from "./routes"
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log("server sis listening at port 3333");
+});
