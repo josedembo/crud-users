29c7784c7aeb87222ca0ccc5ce07d45ec183b1d6
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-09 02:06:37 -0300

    Adiciona o middleware de tratamento de erros
        - importa as routas de usuario no arquivo server(servidor)

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import "./database";
 import express from "express";
-import { routes } from "./routes"
+import { routes } from "./routes";
+import { errorHandle } from "./middleware/GlobalErrors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+app.use(errorHandle)
 
 app.listen(3333, () => {
     console.log("server sis listening at port 3333");
