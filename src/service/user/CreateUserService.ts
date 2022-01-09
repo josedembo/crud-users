b05e4f0fe295075437c509c7be5782ef7889417d
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-09 01:59:55 -0300

    Implementa o tratamento de erros no servico
        - Implementa o tratamento erros na criação ou cadastro de um  novo usuario

--- src/service/user/CreateUserService.ts
+++ src/service/user/CreateUserService.ts
@@ -2,31 +2,36 @@ import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 import { getCustomRepository } from "typeorm";
 import { UserRepositories } from "../../repositories/UserRepositories";
 import { User } from "../../entity/User";
+import { AppError } from "../../models/errors/AppError";
 
 class CreateUserService {
 
     async execute({ name, username, email, password, phone }: CreateUserDTO): Promise<User> {
+        try {
 
-        const userRepositories = getCustomRepository(UserRepositories);
+            const userRepositories = getCustomRepository(UserRepositories);
 
-        const existUser = await userRepositories.find({ where: { username, email } });
+            const existUser = await userRepositories.find({ where: { username, email } });
 
-        if (existUser.length != 0) {
-            throw new Error("user already exists");
+            if (existUser.length != 0) {
+                throw new Error("user already exists");
+            }
+
+            const newUser = userRepositories.create({
+                name,
+                username,
+                email,
+                password,
+                phone
+            });
+
+            const user = await userRepositories.save(newUser);
+
+            return user;
+
+        } catch (error) {
+            throw new AppError("User already exists", 400);
         }
-
-        const newUser = userRepositories.create({
-            name,
-            username,
-            email,
-            password,
-            phone
-        });
-
-        const user = await userRepositories.save(newUser);
-
-        return user;
-
     }
 }
 
