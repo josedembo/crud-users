aca4965aa6cff0f36c698fa7707f7ce2ddfedc46
josedembo <josedembo18@aluno.unilab.edu.br>
2022-01-09 02:03:24 -0300

    Implementa o tratamento de erros no contralador
        - Implementa o tratamento no controllador na criação ou cadastro de um novo usuario

--- src/controller/user/CreateUserController.ts
+++ src/controller/user/CreateUserController.ts
@@ -1,18 +1,23 @@
 import { CreateUserService } from "../../service/user/CreateUserService";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
 class CreateUserController {
 
-    async handle(request: Request, response: Response): Promise<Response> {
+    async handle(request: Request, response: Response, next: NextFunction): Promise<Response | undefined> {
+        try {
+            const { name, username, email, password, phone } = request.body;
 
-        const { name, username, email, password, phone } = request.body;
+            const service = new CreateUserService();
 
-        const service = new CreateUserService();
+            const user = await service.execute({ name, username, email, password, phone });
 
-        const user = await service.execute({ name, username, email, password, phone });
+            return response.status(StatusCodes.CREATED).json(user);
 
-        return response.status(StatusCodes.CREATED).json(user);
+        } catch (error) {
+
+            next(error)
+        }
     }
 }
 
